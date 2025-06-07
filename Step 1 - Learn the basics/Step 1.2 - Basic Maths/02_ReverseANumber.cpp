49704d593582ae805f9da6345f759e705f48596d
Aman <151427250+AmandeepS1ngh@users.noreply.github.com>
2025-06-07 18:24:14 +0530

    Create 02_ReverseANumber.cpp

---
+++ Step 1 - Learn the basics/Step 1.2 - Basic Maths/02_ReverseANumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev_num = 0;
+        while(x!=0){
+            int last_num = x%10; // 3 2 1
+            x = x/10;
+            if (rev_num > INT_MAX / 10 || (rev_num == INT_MAX / 10 && last_num > 7)) {
+                return 0;
+            }
+            if (rev_num < INT_MIN / 10 || (rev_num == INT_MIN / 10 && last_num < -8)) {
+                return 0;
+            }
+            rev_num = (rev_num * 10) + last_num; // 3 32 321 
+        }
+        return rev_num;
+    }
+};
