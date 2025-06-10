13c5727bf7c9db73cf4646fa826a636f9bdefefe
Aman <151427250+AmandeepS1ngh@users.noreply.github.com>
2025-06-10 23:57:56 +0530

    Create r1.cpp

---
+++ Step 1 - Learn the basics/ Step 1.3 - Recursion/r1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void print(int i,int n){
+    if(i>n) return;
+    cout<<i<< " ";
+    print(i+1,n);
+}
+
+int main(){
+    
+    print(1,10);
+
+    return 0;
+}
